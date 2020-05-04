ed605093ba824c31f19dca8c972fcb5098078de9
RP <panwanganan@gmail.com>
2020-05-03 18:39:27 -0700

    frontEnd almost all elements are added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation.js';
 import Logo from './Components/Logo/Logo.js';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './Components/Rank/Rank.js';
 import './App.css'
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 150,
+            density:{
+                enable: true,
+                value_area: 800
+            }
+        }/*,
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 100
+            }
+        }*/
+    }
+};
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Particles className='particles' params={particlesOptions}/>
                 <Navigation />
                 <Logo />
+                <Rank />
                 <ImageLinkForm />
             {//    <FaceRecognition />
 }
